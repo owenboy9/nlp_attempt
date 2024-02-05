0909ac837d3a41534f8f526a7cb8e7c2a027289f
owenboy9 <owenkraus9@gmail.com>
2024-02-05 13:29:20 +0100

    not entirely successful function for grouping bare words into pos groups commit still

--- nlp_attempt.py
+++ nlp_attempt.py
@@ -1,7 +1,7 @@
 # import spacy and the large version of the english dictionary
 import spacy
 nlp = spacy.load("en_core_web_lg")
-# import features responsible for filtering out stop words and puntuation
+# import features responsible for filtering out stop words and punctuation
 from spacy.lang.en.stop_words import STOP_WORDS
 from string import punctuation
 # import the feature that allows word search
@@ -22,9 +22,43 @@ def preprocess_text(text):
     words = []
     # extract lemmatized tokens: no stop words, punctuation nor numbers
     tokens = [token.lemma_ for token in doc if token.is_alpha and token.is_stop and not token.is_punct]
-    # add stripped and lower-cased words to the list
+    # add stripped and lower-cased words to the list, prevent repetitions
     for token in tokens:
         bare = token.lower().strip()
         if bare not in words:
             words.append(bare)
     return words
+
+bare_words = preprocess_text(text)
+for word in bare_words:
+    print(word)
+
+# function for grouping words by parts of speech, using the prepped word list
+def parts(bare_words):
+    # instantiate bare word list as nlp doc
+    doc = nlp(" ".join(bare_words))
+
+    # create a dictionary to store words grouped by parts of speech
+    pos_groups = {}
+
+    for token in doc:
+        # check if token has valid pos
+        if token.pos_:
+            # get part of speech of token
+            pos = token.pos_
+
+            # use word from bare_words without additional processing
+            bare = token.doc
+
+            # add token to its pos group in dictionary
+            if pos not in pos_groups:
+                pos_groups[pos] = [bare]
+            else:
+                pos_groups[pos].append(bare)
+
+    return pos_groups
+
+pos_groups = parts(bare_words)
+
+for pos, words in pos_groups.items():
+    print(f"{pos}: {words}")
