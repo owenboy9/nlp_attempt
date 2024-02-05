037000b86f5c421569e04489b758881f44519c59
owenboy9 <owenkraus9@gmail.com>
2024-02-05 21:04:13 +0100

    shorten text.txt (just tender buttons) incorporate pos filtering into parts() all works now

--- nlp_attempt.py
+++ nlp_attempt.py
@@ -10,7 +10,7 @@ from spacy.matcher import PhraseMatcher
 # import reference material
 text = open('text.txt', 'r', encoding='utf8').read()
 
-# convert reference material into an nlp document
+# convert reference material into nlp document
 doc = nlp(text)
 
 # function for grouping words by parts of speech, using the prepped word list
@@ -20,45 +20,39 @@ def parts(text):
 
     # create a dictionary to store words grouped by parts of speech
     pos_groups = {}
-
+    # run through the whole text
     for token in doc:
-        # check if token has a valid POS
+        # check if token has a valid pos
         if token.pos_:
             # get the part of speech of the token
             pos = token.pos_
-
-            # use the words without additional processing
+            # use the words without additional processing for greater pos accuracy
             bare = token.text
-            # tokens = [token.lemma_ for token in doc if token.is_alpha and token.is_stop and not token.is_punct]
-
-            # if pos doesn't already exist in the dictionary, create its group & add token to it
-            if pos not in pos_groups:
-                pos_groups[pos] = [bare]
-            # otherwise, add it
-            else:
-                pos_groups[pos].append(bare)
-    # return the pos dictionary w/pos groups & tag subgroups
+            # create a list of pos to be excluded from final dictionary & use it as first filter
+            exclude_pos = ["SPACE", "PUNCT", "X", "SYM", "PART", "AUX", "DET", "PROPN", "NUM"]
+            if pos not in exclude_pos:
+                # if pos doesn't already exist in dictionary, create its group & add token to it
+                if pos not in pos_groups:
+                    pos_groups[pos] = [bare]
+                # otherwise, add it to existing pos group
+                else:
+                    pos_groups[pos].append(bare)
+    # return the pos dictionary w/pos groups
     return pos_groups
 
+# press play to get the outcome
 pos_groups = parts(text)
 
 # lemmatize tokens in pos dictionary by making a new one
 lemmatized_pos_groups = {}
-# iterate through every item (word) in pos, recreating the first dictionary structure
+# iterate through every item (word) in every pos, recreating the first dictionary's structure
 for pos, words in pos_groups.items():
-    # but lemmatizing every word (i.e. also performing .strip())
+    # but lemmatizing every word (also turning it to lower case and performing .strip() by default)
     lemmatized_pos_groups[pos] = {nlp(word.lower())[0].lemma_ for word in words}
 
 # in order to print the resulting lemmatized dictionary, convert sets to lists
 for pos, words in lemmatized_pos_groups.items():
     lemmatized_pos_groups[pos] = list(words)
 
-# list undesired pos
-exclude_pos = ["SPACE", "PUNCT", "OTHER", "SYM", "PART", "AUX", "DET", "PROPN", "NUM"]
-
-# filter out undesired POS
-final_pos_groups = {pos: words for pos, words in lemmatized_pos_groups.items() if pos not in exclude_pos}
-
-# print the whole thing with explanations
-for pos, words in final_pos_groups.items():
+for pos, words in lemmatized_pos_groups.items():
     print(f"{spacy.explain(pos)}: {words}\n")
