07a396dfbee07a4304f477718458acd1a15414e7
owenboy9 <owenkraus9@gmail.com>
2024-02-05 11:48:33 +0100

    update readme prep reference material turning it into a list of stripped, lemmatized words, no stop words, no punctuation marks

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-try here# nlp_attempt
+1. build reference material composed of three classic literary works: mrs dalloway, ulysses, and tender buttons. although the app is supposed to spit out poetry, the literary genre of texts used for reference material does not matter, because the app will pick singular words to build poetry with. like bricks.
+2. write a function that will process reference text turning it into a list of "bare" and "relevant" words, that is, no stop words, no punctuation marks, all lower case and lemmatized.
--- nlp_attempt.py
+++ nlp_attempt.py
@@ -1,2 +1,40 @@
+#import spacy and the large version of the english dictionary
 import spacy
 nlp = spacy.load("en_core_web_lg")
+#import features responsible for filtering out stop words and puntuation
+from spacy.lang.en.stop_words import STOP_WORDS
+from string import punctuation
+#import the feature that allows word search
+from spacy.matcher import PhraseMatcher
+
+
+#create a list of elements to cut out of reference material material
+# skip_words = list(STOP_WORDS) + list(punctuation) + ['\n']
+
+#import reference material
+text = open('text.txt', 'r', encoding='utf8').read()
+
+#convert reference material into an nlp document
+doc = nlp(text)
+
+#create a list of words to work with
+# all_words = [word.text for word in doc]
+
+#create a function process reference material, turning it into a list of bare words, lower case, lemmatized
+def preprocess_text(text):
+    # replace newline characters with a space
+    text = text.replace('\n', ' ')
+    doc = nlp(text)
+    #create an empty list to add prepped words to
+    words = []
+    # extract lemmatized tokens without stop words and punctuation
+    tokens = [token.lemma_ for token in doc if not token.is_stop and not token.is_punct]
+    #
+    for token in tokens:
+        bare = token.lower().strip()
+        if bare not in words:
+            words.append(bare)
+
+    print(words)
+
+preprocess_text(text)
