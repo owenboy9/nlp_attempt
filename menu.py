86b47c63a2398a0985a0bfd0681b1eb9dbf5544d
owenboy9 <owenkraus9@gmail.com>
2024-02-10 23:01:56 +0100

    promissing menu ready to incorporate into poem

--- menu.py
+++ menu.py
@@ -7,50 +7,72 @@ nlp = spacy.load("en_core_web_lg")
 
 
 def menu():
-
+    # empty list to store the words in
+    poem = []
     # initiate loop with easy break option
-    run = True
-    while run:
+    while True:
         # clear the console making sure the code can run on either windows or unix
         os.system('cls' if os.name == 'nt' else 'clear')
-        print('welcome to this simple hybrid of a dadaist poet and a pythia\n')
-        print('choose your words carefully, one by one, typing in numbers from 1 to 8:\n')
-        print("1. adjective\n2. noun\n3. pronoun\n4. verb\n5. adposition\n6. adverb\n7. interjection\n8. conjunction\n")
-        # make sure user input is an int & prevent program from crashing
+
+        # display list of options
+        print('_' * 50)
+        print('\nwelcome to this simple hybrid of a dadaist poet and a pythia\n')
+        print('choose your words carefully, one by one, by typing in numbers from 1 to 8:\n')
+        print('1. adjective\n2. noun\n3. pronoun\n4. verb\n5. adposition\n6. adverb\n7. interjection\n8. conjunction\n')
+        print('press enter twice for newline\n')
+        # exit option
+        print('to exit press Q\n')
+        print('_' * 50)
+
+        # space for user input for pos
+        user_input = input()
+
+        # exit option
+        if user_input.lower() == 'q':
+            break
+        # newline option
+        elif user_input == '':
+            poem.append('\n')
+        else:
+            print('not sure what you mean')
+
+        # make sure user input for pos is an int & prevent program from crashing
         try:
-            # user input for pos
-            pos = int(input('1 - 2 - 3 - 4 - 5 - 6 - 7 - 8\n'))
+            pos = int(user_input)
             # make sure user input is within available options
             if 0 < pos <= 8:
-                if pos == 1:
-                    pos_input = 'ADJ'
-                    one = one_word(pos_input)
-                elif pos == 2:
-                    pos_input = 'NOUN'
-                    one = one_word(pos_input)
-                elif pos == 3:
-                    pos_input = 'PRON'
-                    one = one_word(pos_input)
-                elif pos == 4:
-                    pos_input = 'VERB'
-                    one = one_word(pos_input)
-                elif pos == 5:
-                    pos_input = 'CCONJ'
-                    one = one_word(pos_input)
-                elif pos == 6:
-                    pos_input = 'ADV'
-                    one = one_word(pos_input)
-                elif pos == 7:
-                    pos_input = 'INTJ'
-                    one = one_word(pos_input)
-                else:
-                    pos_input = 'ADJ'
-                    one = one_word(pos_input)
+                # convert user input into pos
+                pos_input = convert_number_to_pos(pos)
 
-                go_on = input("\nDo you want to choose more words? (y/n): ").lower()
-                if go_on != 'y':
-                    break
+                # call function to get random word from the specific pos list
+                one = one_word(pos_input)
+
+                # add word to poem
+                poem.append(one)
+                print(f'this is what you have so far: {poem}')
             else:
-                print("Invalid input. Please enter a number between 1 and 8.")
+                print('numbers from 1 to 8 or double enter please')
         except ValueError:
-            print("Invalid input. Please enter a number.")
+            print("invalid input. please enter a number.")
+
+    return poem
+
+
+# function to convert number to part of speech for one_word() to use
+def convert_number_to_pos(number):
+    if number == 1:
+        return 'ADJ'
+    elif number == 2:
+        return 'NOUN'
+    elif number == 3:
+        return 'PRON'
+    elif number == 4:
+        return 'VERB'
+    elif number == 5:
+        return 'ADP'
+    elif number == 6:
+        return 'ADV'
+    elif number == 7:
+        return 'INTJ'
+    elif number == 8:
+        return 'CONJ'
