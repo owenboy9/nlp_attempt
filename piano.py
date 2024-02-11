c1d73de4054ba0da4345bd5f624b41ff3f6b08b3
owenboy9 <owenkraus9@gmail.com>
2024-02-11 10:19:03 +0100

    more tidying

--- piano.py
+++ piano.py
@@ -1,26 +1,24 @@
 import spacy
 import os
-# set TERM environment variable
+# set TERM environment variable because otherwise it's ugly
 os.environ['TERM'] = 'xterm-256color'
 # load eng language model
 nlp = spacy.load("en_core_web_lg")
 # import relevant function
 from menu import menu
 from all_pos_sorting_in_one import sort_and_save
-from all_pos_sorting_in_one import pos_lists
 # import text from file
 text = open('text.txt', 'r', encoding='utf8').read()
 # convert text to nlp
 doc = nlp(text)
 
 
-# call w_b_w functions one by one for as long as user pleases, return poem afterwards
 def piano():
     # call sort_and_save from all_pos_sorting_in_one to create a pos list dictionary
     pos_lists = sort_and_save()
     # capture poem returned by menu()
     final_composition = menu()
-    print('here\'s what you played:\n')
+    print('here\'s what you have played:\n')
     print(' '.join(final_composition))
 
 
